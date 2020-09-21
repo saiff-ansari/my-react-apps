69fe2088a9a48835b06014286369ac5e3a8a03dc
saiff-ansari <saifprodigy@gmail.com>
2020-09-21 12:31:33 +0530

    Updates

---
+++ precache-manifest.0a56125d0d3a1d3103ca3793635c0490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0e494883912306d7b37fca8e23c84c",
+    "url": "/my-react-apps/index.html"
+  },
+  {
+    "revision": "f9eeb8e774f13f6a3b16",
+    "url": "/my-react-apps/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f7672b4edee9dc34fe22",
+    "url": "/my-react-apps/static/css/main.4082e02f.chunk.css"
+  },
+  {
+    "revision": "f9eeb8e774f13f6a3b16",
+    "url": "/my-react-apps/static/js/2.8be49fe6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-react-apps/static/js/2.8be49fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7672b4edee9dc34fe22",
+    "url": "/my-react-apps/static/js/main.86671725.chunk.js"
+  },
+  {
+    "revision": "756f3409f57665d74b21",
+    "url": "/my-react-apps/static/js/runtime-main.81d9bb3c.js"
+  }
+]);
